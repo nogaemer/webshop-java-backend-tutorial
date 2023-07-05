8df83ba25ee5a3d1dda76b45e6f1235038276e6d
BuildTools <unconfigured@null.spigotmc.org>
2023-07-05 14:26:34 +0200

    Setup Project

---
+++ src/main/java/de/nogaemer/java/backend/test/webshop/Application.java
@@ -0,0 +1,14 @@
+package de.nogaemer.java.backend.test.webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
